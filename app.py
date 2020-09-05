50af05fd95d5edb80a189098594292880dd6837f
Claire Hsu <clhsu@mit.edu>
2020-09-05 15:46:42 -0700

    heroku config

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='./build', static_url_path='/')
 
 @app.route('/', methods=['GET'])
 def index():
