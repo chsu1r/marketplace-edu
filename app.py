2534419943215b81858b6140f8f11f891cf55aaa
Claire Hsu <clhsu@mit.edu>
2020-09-05 19:22:00 -0700

    Adding get, add items to the API. nonnull fields for sale item

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import os
+import json
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -16,6 +17,35 @@ def index():
 @app.route('/api/hello-world')
 def get_hello_world():
 	return {'message' : "hello world"}
+
+@app.route('/api/add-item', methods=['POST'])
+def add_item():
+    data = json.loads(request.data)
+    name = data.get("name", "")
+    description = data.get("description", "")
+    cost = data.get("cost", 0)
+    try:
+        item = SaleItem(
+            name=name,
+            description=description,
+            cost=cost,
+            seller_username=seller_username
+        )
+        db.session.add(item)
+        db.session.commit()
+        return {'status':200}
+    except:
+        errors.append("Unable to add item to database.")
+        return {'status':400}
+    
+
+@app.route('/api/get-items', methods=['GET'])
+def get_items():
+    items = {}
+    total_items = db.session.query(SaleItem).all()
+    for item in total_items:
+        items[item.id] = {'name':item.name,'cost':item.cost,'description':item.description,'seller_username':item.seller_username}
+    return items
 
 if __name__ == "__main__":
     # Only for debugging while developing
--- models.py
+++ models.py
@@ -5,14 +5,29 @@ class SaleItem(db.Model):
 	__tablename__ = 'sale_items'
 
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String())
-	cost = db.Column(db.Integer)
+	name = db.Column(db.String(), nullable=False)
+	cost = db.Column(db.Integer, nullable=False)
 	description = db.Column(db.String())
+	seller_username = db.Column(db.String(), nullable=False)
 
-	def __init__(self, name, cost, description):
+	def __init__(self, name, cost, description, seller_username):
 		self.name = name
 		self.cost = cost
 		self.description = description
+		self.seller_username = seller_username
 
-	def __repr__(self):
-		return '<id {}>'.format(self.id)
+
+class User(db.Model):
+	__tablename__ = 'users'
+
+	firebase_id = db.Column(db.String(), nullable=False, primary_key=True)
+	name = db.Column(db.String(), nullable=False)
+	username = db.Column(db.String(), nullable=False)
+	email = db.Column(db.String(), nullable=False)
+
+	def __init__(self, firebase_id, name, username, email):
+		self.firebase_id = firebase_id
+		self.name= name
+		self.username = username
+		self.email = email
+
